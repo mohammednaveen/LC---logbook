1d7f34d06884049b6edbcbf25252c107d95ac077
Mohammed Naveen P A <navsdumbs@gmail.com>
2026-05-08 12:43:30 +0530

    Time: 3 ms (29.71%), Space: 20.2 MB (58.74%) - LeetHub

--- 0112-path-sum/0112-path-sum.py
+++ 0112-path-sum/0112-path-sum.py
@@ -6,15 +6,29 @@
 #         self.right = right
 class Solution:
     def hasPathSum(self, root: Optional[TreeNode], targetSum: int) -> bool:
-        def dfs(node , curSum):
-            if not node:
-                return False
-            
-            curSum += node.val
-
-            if not node.left and not node.right:
-                return curSum == targetSum
-            
-            return (dfs(node.left, curSum) or dfs(node.right, curSum))
         
-        return (dfs(root, 0))
+        # def dfs(root, target):
+        #     if root.left is None and root.right is None:
+        #         return (target-root.val) == 0
+        #     left = dfs(root.left, target-root.left.val)
+        #     right = dfs(root.right, target-root.right.val)
+        #     return left or right
+
+        # if root is None:
+        #     return False
+        
+        # targetSum -= root.val
+
+        # if root.left is None and root.right is None:
+        #      return targetSum == 0
+        
+        # return self.hasPathSum(root.left, targetSum) or self.hasPathSum(root.right, targetSum)
+        
+
+        if root is None:
+            return False
+
+        if not root.left and not root.right and targetSum-root.val == 0:
+            return True
+
+        return self.hasPathSum(root.left, targetSum-root.val) or self.hasPathSum(root.right, targetSum-root.val)
