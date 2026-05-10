49ec96d183344a847064e5399cc98a016dc9a09f
Mohammed Naveen P A <navsdumbs@gmail.com>
2026-05-10 17:35:55 +0530

    Time: 8 ms (78.38%), Space: 31.1 MB (92.14%) - LeetHub

--- 0217-contains-duplicate/0217-contains-duplicate.py
+++ 0217-contains-duplicate/0217-contains-duplicate.py
@@ -1,23 +1,13 @@
-class Solution(object):
-    def containsDuplicate(self, nums):
-        unique = set(nums)
-        return len(unique) != len(nums)
+class Solution:
+    def containsDuplicate(self, nums: List[int]) -> bool:
+        # second approach using len function
+        return len(nums)!=len(set(nums))
 
-
-
-
-
-
-        # Initial Approach
+        # first approach and a proper Hash Table approach used.
         # seen = set()
         # for i in nums:
-        #     if i not in seen:
-        #         seen.add(i)
-        #     else:
+        #     if i in seen:
         #         return True
+        #     else:
+        #         seen.add(i)
         # return False
-        """
-        :type nums: List[int]
-        :rtype: bool
-        """
-        
